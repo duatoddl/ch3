3d89fa88a603022d3855c769a168ffa9b4fa5da9
duatoddl <duatoddl@gmail.com>
2021-04-19 13:52:19 +0900

    test 추가하기 : 따라해봄

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from polls.models import Choice, Question
 from django.views import generic
 from django.template import loader
+from django.utils import timezone
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -12,11 +13,17 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -28,12 +35,12 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html',{
+        return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
         })
     else:
         selected_choice.votes += 1
         selected_choice.save()
-    return HttpResponse(reverse('polls:results', args=(question.id,)))
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
